e6171cf4562a448f81e5f74937961f7a0252b408
kyunggwan <4562sky@naver.com>
2023-03-26 00:40:44 +0900

    feat: CORS 통합

--- board/src/main/java/com/kyunggwan/board/controller/MainController.java
+++ board/src/main/java/com/kyunggwan/board/controller/MainController.java
@@ -5,7 +5,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@CrossOrigin(originPatterns="http://localhost:3000")
 @RestController
 @RequestMapping("/")
 public class MainController {
