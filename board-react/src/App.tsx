eb0ea78f1690ebf0d824c5373e075e14b3c04ef9
kyunggwan <4562sky@naver.com>
2023-03-26 02:26:48 +0900

    feat: 회원가입 구현

--- board-react/src/App.tsx
+++ board-react/src/App.tsx
@@ -1,35 +1,10 @@
-import React, {useState, useEffect} from 'react';
-import logo from './logo.svg';
 import './App.css';
-import axios from 'axios';
-import SignUp from './views/SignUp';
+import Authentication from "./views/Authentication";
 
-function App() {
-  // const [connection, setConnection] = useState<string>('');
-
-  // const connectionTest = () => {
-  //   axios.get('http://localhost:4000/').then((response) => {
-  //     setConnection(response.data);
-  //   }).catch((error) => {
-  //     setConnection((error.message));
-
-  //   })
-  // }
-
-  // useEffect(() => {
-  //   connectionTest();
-  // },[]);
+export default function App() {
 
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-      
-
-      </header>
-        <SignUp />
-    </div>
+    <Authentication />
   );
 }
 
-export default App;
--- board-react/src/views/Authentication/SignUp/index.tsx
+++ board-react/src/views/Authentication/SignUp/index.tsx
@@ -1,34 +1,59 @@
 import axios from 'axios';
 import React, {useState, useEffect} from 'react';
+import Box from '@mui/material/Box';
+import TextField from '@mui/material/TextField';
+import Card from '@mui/material/Card';
+import CardActions from '@mui/material/CardActions';
+import CardContent from '@mui/material/CardContent';
+import { Button } from '@mui/material';
  
 export default function SignUp() {  
-    const [requestResult, setRequestResult] = useState<String>(" ");
+    const [userEmail, setUserEmail] = useState<String>('');
+    const [userPassword, setuserPassword] = useState<String>('');
+    const [userPasswordCheck, setuserPasswordCheck] = useState<String>('');
+    const [userNickname, setuserNickname] = useState<String>('');
+    const [userPhoneNumber, setuserPhoneNumber] = useState<String>('');
+    const [userAddress, setuserAddress] = useState<String>('');
+    const [userAddressDetail, setuserAddressDetail] = useState<String>('');
 
     const signUpHandler = () => {
         const data = {
-                "userEmail": "4562sky@naver.com",
-                "userPassword": "qwerty1234!!",
-                "userPasswordCheck" : "qwerty1234!!",
-                "userNickname": "hannin",
-                "userPhoneNumber": "010-456-456",
-                "userAddress": "부산 금정구",
-                "userAddressDetail": "남산동"
+                "userEmail": userEmail,
+                "userPassword": userPassword,
+                "userPasswordCheck": userPasswordCheck,
+                "userNickname":  userNickname,
+                "userPhoneNumber": userPhoneNumber,
+                "userAddress": userAddress,
+                "userAddressDetail": userAddressDetail
         };
 
         axios.post('http://localhost:4000/api/auth/signUp', data)
         .then((response) => { 
-            setRequestResult('success!!');
+            
         })
         .catch((error) => {
-            setRequestResult('Failed!!');
+            
         })
     };
   return (
-    <div>
-        <h3>{requestResult}</h3>
-        <button onClick={() => signUpHandler()}>회원가입</button>
 
-
-    </div>
+    <Card sx={{ minWidth: 275, maxWidth:"50vw" }}>
+        <CardContent> 
+            <Box>
+                <TextField fullWidth  label="이메일 주소" type = "email" variant="standard" onChange={(e) => setUserEmail(e.target.value)}/>
+                <TextField fullWidth  label="비밀번호" type = "password" variant="standard" onChange={(e) => setuserPassword(e.target.value)}/>
+                <TextField fullWidth  label="비밀번호 확인" type = "password" variant="standard" onChange={(e) => setuserPasswordCheck(e.target.value)}/>
+                <TextField fullWidth  label="닉네임" variant="standard" onChange={(e) => setuserNickname(e.target.value)} />
+                <TextField fullWidth  label="휴대폰 번호" variant="standard" onChange={(e) => setuserPhoneNumber(e.target.value)}/>
+                <TextField fullWidth  label="주소" variant="standard" onChange={(e) => setuserAddress(e.target.value)}/>
+                <TextField fullWidth  label="상세주소" variant="standard"onChange={(e) => setuserAddressDetail(e.target.value)} />
+            </Box>
+        </CardContent>
+            
+        <CardActions>
+            <Button fullWidth onClick={() => signUpHandler()} variant="contained">회원가입</Button>          
+        </CardActions>
+    </Card>
+    
   )
 }
