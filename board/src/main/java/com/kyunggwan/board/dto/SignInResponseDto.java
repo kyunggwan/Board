a8cce9d68c5d2055944e7800fd0ebefe3de98162
kyunggwan <4562sky@naver.com>
2023-03-26 00:40:58 +0900

    rename: 이름 부분 수정

--- board/src/main/java/com/kyunggwan/board/dto/SignInResponseDto.java
+++ board/src/main/java/com/kyunggwan/board/dto/SignInResponseDto.java
@@ -7,7 +7,7 @@ import lombok.NoArgsConstructor;
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
-public class SignUpResponseDto {
+public class SignInResponseDto {
 
 	private String token;
 	private int exprTime;
