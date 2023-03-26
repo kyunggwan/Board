52aba2650fb92c51930b60960c92fbd7bfbea3fe
kyunggwan <4562sky@naver.com>
2023-03-27 01:05:08 +0900

    feat: backend, Spring Security로 인증처리 구현

--- board/src/main/java/com/kyunggwan/board/security/TokenProvider.java
+++ board/src/main/java/com/kyunggwan/board/security/TokenProvider.java
@@ -10,6 +10,13 @@ import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
 
+// JWT: Jason Web Tokken
+// JSON 형태로 된 웹 토큰
+// {header}.{payload}.{signature}로 구성
+
+// header: typ (해당 토큰의 타입), alg (토큰을 서명하기 위해 사용된 해시 알고리즘)
+// payload: sub (해당 토큰의 주인), iat (토큰이 발행된 시간), exp (토큰이 만료되는 시간)
+
 @Service
 public class TokenProvider {
 	// JWT 생성 및 검증을 위한 키
@@ -34,7 +41,7 @@ public class TokenProvider {
 		// 매개변수로 받은 token을 키를 사용해서 복호화 (디코딩)
 		Claims claims = Jwts.parser().setSigningKey(SECURITY_KEY).parseClaimsJws(token).getBody();
 		// 복호화된 토큰의 payload에서 제목을 가져옴
-		return claims.getSubject(); //지정된 subject를 받아 올 수있다.
+		return claims.getSubject(); // 지정된 subject를 받아 올 수있다.
 	}
 
 }
