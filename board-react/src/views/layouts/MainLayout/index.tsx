df8cfcd939a96ec4f16be1bcd15227dc3fe33764
kyunggwan <4562sky@naver.com>
2023-03-27 01:57:46 +0900

    feat: frontend 인증 적용

--- board-react/src/views/layouts/MainLayout/index.tsx
+++ board-react/src/views/layouts/MainLayout/index.tsx
@@ -1,12 +1,41 @@
-import React from 'react'
 import Navigation from '../../Navigation'
 import Authentication from '../../Authentication'
+import BoardMain from '../../BoardMain'
+import { useEffect, useState } from 'react'
+import { useUserStore } from '../../../stores'
+import { useCookies } from 'react-cookie'
+import axios from 'axios'
+import { request } from 'http'
 
 export default function MainLayout() {
+  
+  const [boardResponse, setBoardResponse] = useState<String>('');
+  const [cookies] = useCookies();
+  const { user } = useUserStore();
+
+  const getBoard = async (token: string) => {
+    const requestOption = {headers: {
+      Authorization: `Bearer  ${token}`
+    }
+  };
+    
+  await axios.get('http://localhost:4000/api/board/', requestOption).then((response) => {
+      setBoardResponse(response.data);
+    }).catch((error) => '');
+  }
+
+ useEffect(() => {
+  const token = cookies.token;
+  if(token) getBoard(token);
+ }, [user]);
+
   return (
     <>
         <Navigation />
-        <Authentication />
+
+        {boardResponse ? (<BoardMain />) : (<Authentication />) }
+        
+        
     </>
   )
 }
