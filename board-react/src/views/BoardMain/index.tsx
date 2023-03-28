040a76a2224dad2fcbbaf12bcf47228ba1c9f51e
kyunggwan <4562sky@naver.com>
2023-03-28 20:30:29 +0900

    style: 부분 스타일 수정

--- board-react/src/views/BoardMain/index.tsx
+++ board-react/src/views/BoardMain/index.tsx
@@ -2,6 +2,6 @@ import React from 'react'
 
 export default function BoardMain() {
   return (
-    <div style ={{ height: '100vh', backgroundColor: '#99ff00'}}>Board Main</div>
+    <div>Board Main</div>
   )
 }
